fee103bff4a9bbc3f92324ae2d076b6ade53250d
HarryCandlish <hcnd90@gmail.com>
2019-05-17 11:02:16 +1200

    created nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Books from "./components/books/books";
+import Nav from "./components/nav/Nav";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <header className="App-header" />
         <Books />
       </div>
