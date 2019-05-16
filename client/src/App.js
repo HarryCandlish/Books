f8d1b240756bf37a4b5330fb8322824b3df11c1f
HarryCandlish <hcnd90@gmail.com>
2019-05-17 11:10:49 +1200

    merged with home-page and nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,20 @@ import { HashRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 
-
 import Home from "./components/home/Home";
 import Books from "./components/books/Books";
-
+import Nav from "./components/nav/Nav";
 
 class App extends Component {
   render() {
     return (
-
       <Router>
         <div className="App">
-          <header className="App-header" />
+          <Nav />
           <Route exact path="/" component={Home} />
           <Route exact path="/books" component={Books} />
         </div>
       </Router>
-
     );
   }
 }
